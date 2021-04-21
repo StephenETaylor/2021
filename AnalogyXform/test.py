c2a770c08ecfb66a86d05a0ef1845cb7fbf84ce4
Stephen Eugene Taylor <taylor@kiv.zcu.cz>
2021-04-21 12:29:18 +0200

    small changes to test.py to allow setting up parallel jobs

--- AnalogyXform/test.py
+++ AnalogyXform/test.py
@@ -3,6 +3,17 @@
 This test program sets up parameters for testing the code in subs1.py
 """
 import subs1 as s1
+import sys
+
+if len(sys.argv)>1:
+    P = [int(sys.argv[1])]
+else: 
+    P = [0,  90, 100, 110, 150, 200]
+
+if len(sys.argv)>2:
+    R = int(sys.argv[2])
+else:
+    R = 20
 
 for l in [2]:
     s1.TestDictVer = 1
@@ -10,7 +21,7 @@ for l in [2]:
     
     s1.setup(l)
     s1.relations = s1.A2relations  
-    for p in [0, 1, 9, 10, 11]:
-        s1.Pinned = p*10
-        for i in range(20):
+    for p in P:
+        s1.Pinned = p
+        for i in range(R):
             s1.main()
