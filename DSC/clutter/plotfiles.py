a9f7087f83cbf24ce6f3e5e279267a128ba0ac11
Stephen Taylor <stepheneugenetaylor@gmail.com>
2021-04-24 22:13:45 +0200

    add separate graphs for BINAry and RANK tasks

--- DSC/clutter/plotfiles.py
+++ DSC/clutter/plotfiles.py
@@ -21,45 +21,78 @@ files = [
 ,'Results-w2v-skipgram-Ne-Re-cca-java.dat']]
 
 def main():
+    with open('plotfiles.txt') as fi:
+        comments = dict()
+        key = None
+        text = ''
+        for lin in fi:
+            if len(lin)>10 and lin.find(' ')<0:
+                #new key.  Save old text to old key
+                if key is not None:
+                    comments[key] = text
+                # set up for this key
+                key = lin.strip()
+                text = ''
+            else:   # not a key.  Add line to text
+                text += lin
+
     with open('report.htm','w') as fo:
         with open('make.gnuplots','w') as fg:
             fo.write('<html><body>\n')
             for fl in files:
-                title = fl[0]
+                title = fl[0][:-4] # drop extension
                 e1 = title.find('B')
-                if e1 < 0: raise Exception('oops')
-                title = title[:e1-1]
+                if e1 < 0: 
+                    e1 = title.find('N')
+                if e1 < 0: 
+                    raise Exception('oops')
+                title = title[:e1]+title[e1+6:]
                 fo.write('<h1>')
                 fo.write(title)
                 fo.write('</h1>')
-                pic = makeg(title,fl,fg)
+                pic1,pic2 = makeg(title,fl,fg)
+                fo.write("""
+                    <table><tr><td>""")
                 fo.write('<img src="')
-                fo.write(pic)
+                fo.write(pic1)
                 fo.write('">\n')
+                fo.write('<td><img src="')
+                fo.write(pic2)
+                fo.write('">\n</table>')
                 fo.write('<br/>\n')
+                comm =  comments.get(title,None)
+                if comm is not None:
+                    fo.write('<par>\n%s\n</par>\n'%comm)
+
             fo.write('</body></html>\n')
 
 def makeg(pname, flist, handle):
-    fname = pname+'.png'
-    handle.write('set terminal push\n')
-    handle.write('set terminal pngcairo\n')
-    handle.write('set output "')
-    handle.write(fname)
-    handle.write('"\n')
+    fname = ['BINA-'+pname+'.png','RANK-'+pname+'.png']
+    for j in range(2):
+        handle.write('set terminal push\n')
+        handle.write('set terminal pngcairo\n')
+        handle.write('set output "')
+        handle.write(fname[j])
+        handle.write('"\n')
 
-    for i,fna in enumerate(flist):
+        for i,fna in enumerate(flist):
 
-        if i == 0:
-            handle.write('plot "')
-        else:
-            handle.write(', "')
-        handle.write(fna)
-        handle.write('" using 1:2:3 with errorbars, "" with lines')
-    handle.write('\n')
-    handle.write('\nset output\n')
-    handle.write('set terminal pop\n')
+            if i == 0:
+                handle.write('plot "')
+            else:
+                handle.write(', "')
+            handle.write(fna)
+            if j==0:
+                handle.write(
+                  '" using 1:2:3 with errorbars, "" using 1:2:3 with lines')
+            else:
+                handle.write(
+                  '" using 1:16:17 with errorbars, "" using 1:16:17 with lines')
+        handle.write('\n')
+        handle.write('\nset output\n')
+        handle.write('set terminal pop\n')
 
-    return fname
+    return fname[0], fname[1]
 
 
                     
